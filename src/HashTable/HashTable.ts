66d66e40460947dea2cc55bddc0a2532f77b3c33
sagar <sgavhane70@gmail.com>
2020-04-12 12:42:06 +0530

    wip"

--- src/HashTable/HashTable.ts
+++ src/HashTable/HashTable.ts
@@ -1,26 +1,11 @@
-import LinkedList from './../LinkedList'
-
 type MyKeyType = string | number
 type ValueType = string | number | object | Array<string | number | object>
-
-const DEFAULT_HASH_TABLE_CAPACITY = 32
 
 class HashTable {
   #buckets: Array<ValueType> = []
   #keys: any = {}
-  #capacity: number = DEFAULT_HASH_TABLE_CAPACITY
 
-  constructor(capacity: number = DEFAULT_HASH_TABLE_CAPACITY) {
-    const absoluteCapacity = Math.abs(capacity)
-    this.#capacity =
-      absoluteCapacity > 0 ? absoluteCapacity : DEFAULT_HASH_TABLE_CAPACITY
-    this.#buckets = Array(this.#capacity)
-      .fill(null)
-      .map(() => new LinkedList())
-  }
-
-  // todo: hashCode method should be private
-  public hashCode(key: MyKeyType): number {
+  private hashCode(key: MyKeyType): number {
     let i, h
 
     for (i = 0, h = 0; i < `${key}`.length; i++)
@@ -29,22 +14,34 @@ class HashTable {
     return h
   }
 
-  set(key: MyKeyType, _value: ValueType) {
-    const keyHash = this.hashCode(key)
+  set(key: MyKeyType, value: ValueType) {
+    const keyHash: any = `${this.hashCode(key)}`.toString()
+    const tuple: object = { [key]: value }
+    const bucket: any = this.#buckets.find((bucket) => bucket === keyHash)
 
-    this.#keys[key] = keyHash
-    let bucket: any = this.#buckets[keyHash]
-
-    if (bucket) {
-      // const node = bucket.find({  })
+    if (!bucket) {
+      this.#buckets.push(keyHash)
+      this.#keys[keyHash] = { tuple }
+    } else {
+      this.#keys[bucket] = { tuple }
     }
-    console.log('#buckets', this.#buckets)
-    console.log('bucket', bucket)
-    console.log('k', this.#keys)
+  }
+
+  get(key: MyKeyType) {
+    const keyHash: any = `${this.hashCode(key)}`.toString()
+    const bucket: any = this.#buckets.find((bucket) => bucket === keyHash)
+
+    if (!bucket) return undefined
+
+    return this.#keys[keyHash].tuple
   }
 }
 
 export default HashTable
-// const ht = new HashTable(10)
+
+// const ht = new HashTable()
 // ht.set('name', 'mike')
-// console.log('[code]', code)
+
+// // eslint-disable-next-line sonarjs/no-element-overwrite
+// ht.set(900, 'jake')
+// console.log(ht.get(900))
